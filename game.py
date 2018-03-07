00dd11991bf83c73eb3fb53c095a01f172b2c473
july <2363634155@qq.com>
2018-03-07 14:20:10 +0800

    main arch

--- game.py
+++ game.py
@@ -2,4 +2,48 @@
 # -*- coding:utf-8 -*-
 
 import pygame
+#init pygame
+pygame.init()
+
+#set game screen size,background img,caption etc
+
+#set screen
+screen = pygame.display.set_mode((480,800))
+
+#set caption
+
+pygame.display.set_caption("plane shoot")
+
+#set background image
+background = pygame.image.load("resources/image/background.png").convert()
+
+#get game plane image
+plane_imgs = pygame.image.load("resources/image/shoot.png")
+
+#get player plane image
+player = plane_imgs.subsurface(pygame.Rect(0,99,102,126))
+
+#main circle to handle init,update,exit
+
+while True:
+    #init screen
+    screen.fill(0)
+    screen.blit(background,(0,0))
+    #display player plane location
+    screen.blit(player,[200,600])
+    #update screen
+    pygame.display.update()
+
+    #exit
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+
+
+
+
+
 
